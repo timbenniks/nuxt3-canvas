74c592c6a383c81bf934aaa5726cf9413c918049
Tim Benniks <tbenniks@gmail.com>
2021-10-21 18:17:20 +0200

    first commit

---
+++ app.vue
@@ -0,0 +1,23 @@
+<template>dd</template>
+
+<script>
+import { CanvasClient } from "@uniformdev/canvas";
+//import { useNuxtApp } from "#app";
+
+export default {
+  async setup() {
+    const { $config } = useNuxtApp();
+
+    const client = new CanvasClient({
+      apiKey: $config.UNIFORM_API_KEY,
+      projectId: $config.UNIFORM_PROJECT_ID,
+    });
+
+    const { composition } = await client.getCompositionBySlug({
+      slug: "/about",
+    });
+
+    return { composition };
+  },
+};
+</script>
